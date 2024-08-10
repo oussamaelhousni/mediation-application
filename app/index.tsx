16a2896ae646e4e20c59e81a0592596cd00a71e4
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2024-08-10 16:29:14 +0100

    add: initial screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,36 @@
 import { View, Text, ImageBackground } from "react-native";
 import React from "react";
 import beachImage from "@/assets/meditation-images/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import { StatusBar } from "expo-status-bar";
 const App = () => {
   return (
     <View className="grow flex">
       <ImageBackground source={beachImage} resizeMode="cover" className="grow">
-        <Text>Hello word</Text>
+        <LinearGradient
+          colors={["rgba(0,0,0,.4)", "rgba(0,0,0,.8)"]}
+          className="grow"
+        >
+          <SafeAreaView className="my-8 flex justify-between grow px-6">
+            <View>
+              <Text className="text-white text-4xl text-center">
+                Simple meditation
+              </Text>
+              <Text className="text-white text-center text-base">
+                Simplifying mediation for everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                title="Get started"
+                onPress={() => console.log("hi2")}
+              />
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
       </ImageBackground>
     </View>
   );
